63d1701ca3bf2672285319724f3273bfbb304f9f
Dustin Johnson <djohnson@machenergy.com>
2017-08-13 12:06:43 -0500

    Split modal out into a reusable wrapper component. Allow specifying and passing a deck name into creating decks.

--- src/components/AddDeckModal.js
+++ src/components/AddDeckModal.js
@@ -1,50 +1,35 @@
 import React from 'react';
-import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import {Input, InputGroup} from 'reactstrap';
 import '../styles/CollectionPage.css';
+import ModalWrapper from "./ModalWrapper";
 
 class AddDeckModal extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            modal: false
-        };
+        this.state = {name: ''};
 
+        this.handleChange = this.handleChange.bind(this);
         this.deckConfirmed = this.deckConfirmed.bind(this);
-        this.toggle = this.toggle.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({name: event.target.value});
     }
 
     deckConfirmed() {
-        const name = `Deck${new Date().getTime()}`;
+        const name = this.state.name;
 
         this.props.addDeck(name);
-
-        this.toggle();
-    }
-
-    toggle() {
-        this.setState({
-            modal: !this.state.modal
-        });
     }
 
     render() {
         return (
             <div>
-                <Button color="primary" onClick={this.toggle}>Create Deck</Button>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-                    <ModalBody>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={this.deckConfirmed}>Do Something</Button>{' '}
-                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                    </ModalFooter>
-                </Modal>
+                <ModalWrapper title="Create Deck" confirmAction={this.deckConfirmed}>
+                    <InputGroup>
+                        <Input placeholder="deck name" value={this.state.name} onChange={this.handleChange}/>
+                    </InputGroup>
+                </ModalWrapper>
             </div>
         );
     }
---
+++ src/components/ModalWrapper.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import '../styles/CollectionPage.css';
+
+class ModalWrapper extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
+
+        this.confirmed = this.confirmed.bind(this);
+        this.toggle = this.toggle.bind(this);
+    }
+
+    confirmed() {
+        this.props.confirmAction();
+        this.toggle();
+    }
+
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Button color="primary" onClick={this.toggle}>Create Deck</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>{this.props.title}</ModalHeader>
+                    <ModalBody>
+                        {this.props.children}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.confirmed}>Do Something</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default ModalWrapper;
